64c8b18088d52031947b342d4d9028b711af4d14
NadyaCodes <nadyacorscadden@gmail.com>
2022-03-08 22:43:19 -0500

    iterating over an array with while

--- packing-list.js
+++ packing-list.js
@@ -1,7 +1,14 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
-for (let i = 0; i < packingList.length; i++) {
+/* for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
-}
+} */
 
 // use packingList[3]; to access a specific string
+
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
